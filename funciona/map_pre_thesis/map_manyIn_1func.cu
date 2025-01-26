aecf9b033e193ce07f71aa79cdd67d24e975e3e6
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2025-01-26 13:34:07 +0000

    map Many inputs

--- funciona/map_pre_thesis/map_manyIn_1func.cu
+++ funciona/map_pre_thesis/map_manyIn_1func.cu
@@ -47,14 +47,21 @@ void map_impl(Iterator& container, Func& func) {
     size_t bytes = size * sizeof(T);
     
     cudaMalloc(&d_array, bytes);
-    cudaMemcpy(d_array, container.data(), bytes, cudaMemcpyHostToDevice);
+    cudaError_t err = cudaMemcpy(d_array, container.data(), bytes, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA memcpy to device failed: " << cudaGetErrorString(err) << std::endl;
+    }
+
     
     int blockSize = 256;
     int numBlocks = (size + blockSize - 1) / blockSize;
     
     mapKernel<<<numBlocks, blockSize>>>(d_array, size, func);
     cudaDeviceSynchronize();
-    cudaMemcpy(container.data(), d_array, bytes, cudaMemcpyDeviceToHost);
+    cudaError_t err2 = cudaMemcpy(container.data(), d_array, bytes, cudaMemcpyDeviceToHost);
+    if (err2 != cudaSuccess) {
+        std::cerr << "CUDA memcpy to host failed: " << cudaGetErrorString(err2) << std::endl;
+    }
     cudaFree(d_array);
     
 }
@@ -74,27 +81,28 @@ void map_impl(Iterator& container, Func& func, Iterator& output) {
     
     mapKernel<<<numBlocks, blockSize>>>(d_array, size, func);
     cudaDeviceSynchronize();
+    cudaError_t kernelErr = cudaGetLastError();
+    if (kernelErr != cudaSuccess) {
+        std::cerr << "CUDA kernel launch error: " << cudaGetErrorString(kernelErr) << std::endl;
+    }
     cudaMemcpy(output.data(), d_array, bytes, cudaMemcpyDeviceToHost);
     cudaFree(d_array);
     
 }
 
-template <typename ...Args>
-void map(Args &...args) {
-    if constexpr (sizeof...(args) == 2) {
-        map_clean(args...);
-    }
-    else {
-    }
 
-}
 
 
 template <typename Iterator, typename Func>
 void map_clean(Iterator& container, Func& func) {
+    //std::cout << "map_clean\n";
+    //std::cout << "container size: " << container.size() << "\n";
+    //std::cout << "container type: " << typeid(container).name() << "\n";
+
     using T = typename Iterator::value_type;
     std::vector<T> temp; 
     if constexpr (!std::is_same_v<Iterator, std::vector<T>>){
+
         for (auto it = container.begin(); it != container.end(); ++it) {
             temp.push_back(*it);
         }
@@ -106,23 +114,32 @@ void map_clean(Iterator& container, Func& func) {
     }
 }
 
-//Assumes input and output have the same size and type
-template <typename Iterator, typename Func>
-void map(Iterator& container, Func& func, Iterator& output) {
-    using T = typename Iterator::value_type;
-    std::vector<T> temp; 
-    if constexpr (!std::is_same_v<Iterator, std::vector<T>>){
-        for (auto it = container.begin(); it != container.end(); ++it) {
-            temp.push_back(*it);
-        }
-        map_impl(temp, func);
-        std::copy(temp.begin(), temp.end(), output.begin());
-    }
-    else{
-        map_impl(container, func, output);
+template <typename Func, typename Tuple, std::size_t Index = 0>
+void apply_map_clean_to_tuple(Func& func, Tuple& argsTuple) {
+    if constexpr (Index < std::tuple_size_v<Tuple> - 1) { 
+        auto& container = std::get<Index>(argsTuple);     
+        map_clean(container, func);                       
+        apply_map_clean_to_tuple<Func, Tuple, Index + 1>(func, argsTuple); 
     }
 }
 
+//vi && no raptor -> estudar melhor r-value reference
+template <typename ...Args>
+void map(Args &&...args) {
+    if constexpr (sizeof...(args) == 2) {
+        map_clean(args...);
+    }
+    else {
+        
+        auto argsTuple = std::forward_as_tuple(args...);
+
+        constexpr size_t numArgs = sizeof...(args);
+        auto& func = std::get<numArgs - 1>(argsTuple);
+
+        apply_map_clean_to_tuple(func, argsTuple);
+    }
+
+}
 
 template <typename Container, typename Func>
 void cpuMap(Container& container, Func func) {
@@ -165,6 +182,8 @@ int main() {
     const size_t N = 10000000;  
 
     //N elementos com 2.0f
+    std::vector<float> cudaVec_solo(N, 2.0f);
+
     std::vector<float> cudaVec(N, 2.0f);
     std::vector<float> cpuVec(N, 2.0f);
     std::list<float> cudaVecList(N, 2.0f);
@@ -183,18 +202,23 @@ int main() {
     auto endCudaList_out = std::chrono::high_resolution_clock::now();
     auto cudaDurationList_out = std::chrono::duration_cast<std::chrono::milliseconds>(endCudaList_out - startCudaList_out);
 */
-
+    auto startCuda_solo = std::chrono::high_resolution_clock::now();
+    map(cudaVec_solo, IntensiveComputation());
+    auto endCuda_solo = std::chrono::high_resolution_clock::now();
+    auto cudaDuration_solo = std::chrono::duration_cast<std::chrono::milliseconds>(endCuda_solo - startCuda_solo);
     
     auto startCuda = std::chrono::high_resolution_clock::now();
-    map(cudaVec, IntensiveComputation());
+    map(cudaVec ,cudaVecList, IntensiveComputation());
     auto endCuda = std::chrono::high_resolution_clock::now();
     auto cudaDuration = std::chrono::duration_cast<std::chrono::milliseconds>(endCuda - startCuda);
 
+/*
     auto startCudaList = std::chrono::high_resolution_clock::now();
     map(cudaVecList, IntensiveComputation());
     auto endCudaList = std::chrono::high_resolution_clock::now();
     auto cudaDurationList = std::chrono::duration_cast<std::chrono::milliseconds>(endCudaList - startCudaList);
 
+*/
     
     auto startCpu = std::chrono::high_resolution_clock::now();
     std::transform(cpuVec.begin(),cpuVec.end(),cpuVec.begin(), CpuIntensiveComputation);
@@ -205,7 +229,10 @@ int main() {
     compareAndPrint("cudaVec", cudaVec, "cpuVec", cpuVec, "Map", cudaDuration.count(), cpuDuration.count());
 
     // Compare cudaVecList and cpuVec
-    compareAndPrint("cudaVecList", cudaVecList, "cpuVec", cpuVec, "Map List", cudaDurationList.count(), cpuDuration.count());
+    compareAndPrint("cudaVecList", cudaVecList, "cpuVec", cpuVec, "Map List", cudaDuration.count(), cpuDuration.count());
+
+    compareAndPrint("cudaVecSolo", cudaVec_solo, "cpuVec", cpuVec, "Map", cudaDuration_solo.count(), cpuDuration.count());
+
 /*
     // Compare cudaVecWithOutput and cpuVec
     compareAndPrint("cudaVecWithOutput", cudaVec, "cpuVec", cpuVec, "Map", cudaDuration_out.count(), cpuDuration.count());
